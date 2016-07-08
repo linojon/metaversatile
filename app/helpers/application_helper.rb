81cf97f1617ffaf7b0d805dfe386e80dd847f7dc
Jonathan Linowes <jonathan@parkerhill.com>
2016-07-08 12:22:39 -0400

    restore buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,18 @@
 module ApplicationHelper
 
+  # Bootstrap default styles
+  # alert-info      blue
+  # alert-success   green
+  # alert-warning   yellow
+  # alert-danger    red
     def flash_class_for(level)
     'alert alert-dismissable ' +
       case level
-      when 'success' then 'alert-success' # green
-      when 'notice'  then 'alert-info'    # blue
-      when 'alert'   then 'alert-warning' # yellow
-      when 'error'   then 'alert-warning' # yellow ('alert-danger' - red)
-      else                'alert-info'    # blue
+      when 'success' then 'alert-success'
+      when 'notice'  then 'alert-success'
+      when 'alert'   then 'alert-warning'
+      when 'error'   then 'alert-warning'
+      else                'alert-success'
       end
   end
   
